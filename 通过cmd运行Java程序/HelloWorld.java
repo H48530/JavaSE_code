f8174156b53fe9b4524b54e011bf20281d60dd10
H48530 <485301267@qq.com>
2020-12-25 19:42:25 +0800

    源代码

---
+++ 通过cmd运行Java程序/HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+*时间2020年12月25日19:09:54
+*/
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+		
+		//在这里输出HelloWorld，并换行
+        System.out.println("HelloWorld！");
+		
+    }
+}
