5f638e1de34bf5666925e9bad8d5c2b55d6cdc85
H48530 <485301267@qq.com>
2020-12-26 15:24:58 +0800

    字符类型

---
+++ 8种基本数据类型/CharType.java
@@ -0,0 +1,11 @@
+public class CharType {
+
+    //char 字符变量
+    public static void main(String[] args) {
+        char ch1 ='a';
+        char ch2 ='胡';
+        System.out.println(ch1);
+        System.out.println(ch2);
+
+    }
+}
