49a7c70d40419b6c9f34dc14b5c3292b6418ef49
H48530 <485301267@qq.com>
2021-01-23 20:17:48 +0800

    面向对象的思维实现二分查找（一维数组）测试

---
+++ 面向对象的思维实现二分查找/BinarySearchTest.java
@@ -0,0 +1,11 @@
+/**
+ * 二分查找的测试
+ */
+public class BinarySearchTest {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int a = BinarySearch.binarySearch(arr, 5);
+        System.out.println(a);
+    }
+}
