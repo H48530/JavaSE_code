4b938b8cdd37c56d53667efdd42e92431a398a0e
H48530 <485301267@qq.com>
2021-01-24 21:08:02 +0800

    查找有序的二维数组

---
+++ binary_Serch/Test.java
@@ -0,0 +1,20 @@
+package binary_Serch;
+import java.util.Arrays;
+
+public class Test {
+    public static void main(String[] args) {
+        int[][] array = {
+                {1, 2, 3, 4, 5},
+                {6, 7, 8, 9, 10},
+                {11, 12, 13, 14, 15}
+        };
+        int rows = 3;
+        int columns = 5;
+
+        for (long target = 0; target <= 16; target++) {
+            int[] index = binary_Search.search(array, rows, columns, target);
+
+            System.out.printf("查找 %d 的结果是: %s\n", target, Arrays.toString(index));
+        }
+    }
+}
